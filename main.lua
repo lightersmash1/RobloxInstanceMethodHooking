7d7cb5eeb186098c856aaaf109c3f63f98db6902
lightersmash1 <76565273+lightersmash1@users.noreply.github.com>
2024-03-13 01:31:58 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ local FakeInstHooks = {
 		["Instance.Name"] = function(Object) return "LOL" end
 	},
 	newindex = {
-		
+
 	}
 }
 
@@ -25,15 +25,15 @@ local function MakeFakeInstance(Object)
 	local IsFakeInstance, _ = pcall(function()
 		local abc = Object["_instance"]
 	end)
-	
+
 	if IsFakeInstance then
 		return Object
 	end
-	
+
 	local Class = Object.ClassName
 	local FakeInstance = newproxy(true)
 	local mt = getmetatable(FakeInstance)
-	
+
 	mt.__index = function(self, key)
 		if key == "_instance" then
 			return Object
@@ -55,7 +55,7 @@ local function MakeFakeInstance(Object)
 				local Arguments = {...}
 				Arguments[1] = Object
 				for I, V in pairs (Arguments) do
-					if typeof(V) == "Instance" then
+					if type(V) == "userdata" then
 						-- Convert it back
 						pcall(function()
 							Arguments[I] = V["_instance"]
@@ -99,11 +99,11 @@ local function MakeFakeInstance(Object)
 		end
 		return Result
 	end
-	
+
 	mt.__newindex = function(self, key, value)
 		local NewValue = value
-		local ValType = typeof(value)
-		if ValType == "Instance" then
+		local ValType = type(value)
+		if ValType == "userdata" then
 			pcall(function()
 				NewValue = NewValue["_instance"]
 			end)
@@ -119,22 +119,73 @@ local function MakeFakeInstance(Object)
 
 		Object[key] = NewValue
 	end
-	
+
 	mt.__metatable = "The metatable is locked"
-	
+
 	return FakeInstance
+end
+
+function MakeFakeInstFunc(Result)
+	return function(...)
+		local Arguments = {...}
+		for I, V in pairs (Arguments) do
+			if type(V) == "userdata" then
+				-- Convert it back
+				pcall(function()
+					Arguments[I] = V["_instance"]
+				end)
+			elseif typeof(V) == "table" then
+				Arguments[I] = LoopAllTBLs(V, function(Value)
+					if typeof(Value) == "Instance" then
+						local Inst = Value
+						pcall(function()
+							Inst = V["_instance"]
+						end)
+						return Inst
+					end
+					return Value
+				end)
+			end
+		end
+		local Res = Result(unpack(Arguments))
+		local ResType = typeof(Res)
+		if ResType == "Instance" then
+			return MakeFakeInstance(Res)
+		elseif ResType == "table" then
+			return LoopAllTBLs(Res, function(V)
+				if typeof(V) == "Instance" then
+					return MakeFakeInstance(V)
+				end
+				return V
+			end)
+		end
+		return Res
+	end
 end
 
 function InitializeHooksInCurrentEnvironment()
 	-- why is roblox removing setfenv (there's no alternative to it)
-	
+
 	local CurrentEnvironment = getfenv(2)
-	
+
 	local NewEnvironment = setmetatable({}, {
 		__index = function(self, key)
-			local Result = rawget(self, key) or CurrentEnvironment[key]
+			local Result = CurrentEnvironment[key]
 			if typeof(Result) == "Instance" then
 				return MakeFakeInstance(Result)
+			end
+			if typeof(Result) == "function" then
+				return MakeFakeInstFunc(Result)
+			end
+			if typeof(Result) == "table" then
+				return LoopAllTBLs(Result, function(func)
+					if typeof(func) == "function" then
+						return MakeFakeInstFunc(func)
+					end
+				end)
+			end
+			if Result == nil then
+				Result = rawget(self, key)
 			end
 			return Result
 		end,
@@ -142,9 +193,9 @@ function InitializeHooksInCurrentEnvironment()
 			rawset(self, key, value)
 		end,
 	})
-	
+
 	setfenv(2, NewEnvironment)
-	
+
 end
 
 function test()
